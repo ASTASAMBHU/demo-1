3004f00aa7ffef0b8afb0ce0bf8c9babb024ca4a
astasambhu mohanty <astasambhu97@gmail.com>
2023-02-20 10:37:40 +0530

    Changes in Main.js and responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
   var btn = $("#top-button");
 
+
+  
+  let modifyBtn = $("#modify-btn");
+  // let filterSection = $('#filter-section');
+
+  // $("div.filter").click(function(){
+  //   console.log('Filter Section Selected');
+  // })
+
+  
+  modifyBtn.click(function(){
+    console.log('click');
+    $("div.filter").toggle(600);    
+  });
+
+
+
   $(window).scroll(function () {
     if ($(window).scrollTop() > 300) {
       btn.addClass("show");
@@ -13,4 +30,6 @@ $(document).ready(function () {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "300");
   });
+
+ 
 });
